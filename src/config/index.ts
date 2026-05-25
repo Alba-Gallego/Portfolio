7096dac99988574ac4356e93dcb7894918b1136e
AlvaroGallegoDAW <a.gallego@sapalomera.cat>
2026-05-25 16:07:25 +0200

    Fixed canonical url

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,7 +16,7 @@ export const SITE_CONFIG: SiteConfig = {
     { text: "Github", href: "https://github.com/Alba-Gallego" }
   ],
   socialImage: "/alba-og.png",
-  canonicalURL: "https://astro-zen.vercel.app",
+  canonicalURL: "https://portfolio-private-three.vercel.app/",
 };
 
 export const SITE_CONTENT: SiteContent = {
